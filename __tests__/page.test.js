b02059c5006b24b06e5ce0fcbe34da1393785768
Thomas Sanlis <Thomas.sanlis@gmail.com>
2025-09-05 10:52:16 +0200

    Fix prettier

--- __tests__/page.test.js
+++ __tests__/page.test.js
@@ -1,17 +1,17 @@
-import { render, screen } from '@testing-library/react'
-import Home from '../src/app/page'
+import { render, screen } from "@testing-library/react";
+import Home from "../src/app/page";
 
-jest.mock('next/navigation', () => ({
+jest.mock("next/navigation", () => ({
   useRouter() {
     return {
-      prefetch: () => null
-    }
-  }
-}))
+      prefetch: () => null,
+    };
+  },
+}));
 
-describe('Home', () => {
-  it('renders without crashing', () => {
-    render(<Home />)
-    expect(screen.getByText(/get started/i)).toBeInTheDocument()
-  })
-})
+describe("Home", () => {
+  it("renders without crashing", () => {
+    render(<Home />);
+    expect(screen.getByText(/get started/i)).toBeInTheDocument();
+  });
+});
